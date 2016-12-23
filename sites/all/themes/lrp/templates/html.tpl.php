a32ae005724f217fed89f985705096729ccb147f
Mark Lawson <mlawson3691@gmail.com>
2016-12-23 10:07:33 -0800

    move template files to template directory and add meta tag to <head> for mobile responsiveness

--- sites/all/themes/lrp/templates/html.tpl.php
+++ sites/all/themes/lrp/templates/html.tpl.php
@@ -48,6 +48,7 @@
 
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
